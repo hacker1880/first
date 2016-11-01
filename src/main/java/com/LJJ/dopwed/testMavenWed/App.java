46d7abb1c537c827412111ea059c3c047c4599ba
hacker1880 <hacker1880@163.com>
2016-11-01 16:04:22 +0800

    Update App.java

--- src/main/java/com/LJJ/dopwed/testMavenWed/App.java
+++ src/main/java/com/LJJ/dopwed/testMavenWed/App.java
@@ -1,13 +1,1 @@
-package com.LJJ.dopwed.testMavenWed;
-
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
-}
+http://www.tuicool.com/articles/fArInam
